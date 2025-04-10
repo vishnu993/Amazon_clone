54954377895d7b7d1dee19e37b9c64730daf6fdb
vishnu <vishnuvishnu0404@gmail.com>
2025-04-10 05:59:35 -0700

    add script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelectorAll('.control_prev');
+const next_btn = document.querySelectorAll('.control_next');
+
+let n=0;
+
+function changeslid(){
+    for (let i = 0; i < img.length; i++) {
+        imgs[i].display = 'none';
+        
+    }
+        
+    
+}
+changeslid();
