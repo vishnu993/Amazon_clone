494de00315f1f39e938a0b5b5627ca32c615b1cf
vishnu <vishnuvishnu0404@gmail.com>
2025-04-11 05:50:21 -0700

    add products

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const imgs = document.querySelectorAll('.header-slider ul img');
 const prev_btn = document.querySelector('.control_prev');
 const next_btn = document.querySelector('.control_next');
- let n=0;
+let n=0;
 function changeSlide(){
     for (let i = 0; i < imgs.length; i++) {
         imgs[i].style.display = 'none';
@@ -10,16 +10,14 @@ function changeSlide(){
     
 }
 changeSlide();
-prev_btn.addeventlistener('click' , (e)=>{
+prev_btn.addEventListener('click' , (e)=>{
      if(n > 0){
         n--;
        }else{
          n =imgs.length - 1;
        }
-changeSlide();
-
-    })
-    next_btn.addeventlistener('click' , (e)=>{
+changeSlide();    })
+    next_btn.addEventListener('click' , (e)=>{
         if(n < imgs.length - 1){
            n++;
           }else{
